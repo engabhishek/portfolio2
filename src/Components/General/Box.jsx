13ff96e701fa0f0faf59222555d91032a46869b0
Abhishek Sharma <abhisheksharma879150@gmail.com>
2025-03-14 22:43:55 +0530

    text size change in box

--- src/Components/General/Box.jsx
+++ src/Components/General/Box.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Box = (data) => {
   return (
     <>
-    <div className='box bg-blue-950 inline-block py-1 px-2 rounded-md text-white text-xl '>
+    <div className='box bg-blue-950 inline-block py-1 px-2 rounded-md text-white text-lg '>
         <p>{data.text}</p>
     </div>
     </>
