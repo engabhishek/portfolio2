9282a86f6c2577b5ac16af3ffbe5bcb70cf67078
Abhishek Sharma <abhisheksharma879150@gmail.com>
2025-03-14 22:28:03 +0530

    create section 2

--- src/Components/Section2.jsx
+++ src/Components/Section2.jsx
@@ -1,11 +1,33 @@
-import React from 'react'
+import React from "react";
+import H1 from "./General/H1";
+import Box from "./General/Box";
 
 const Section2 = () => {
   return (
     <>
-    
+      <section className="section2">
+      <div className=" p-6 text-center">
+        <H1 text={"MY Skills"} />
+        <div className="mt-2 my-4">
+          <div className="boxItems flex justify-center gap-3 mt-2">
+            <Box text={"JavaScript"} />
+            <Box text={"React.js"} />
+            <Box text={"Git & GitHub"} />
+          </div>
+          <div className="boxItems flex justify-center gap-3 mt-2">
+            <Box text={"CSS"} />
+            <Box text={"HTML"} />
+          </div>
+          <div className="boxItems flex justify-center gap-3 mt-2">
+            <Box text={"C"} />
+            <Box text={"Python"} />
+            <Box text={"Java"} />
+          </div>
+        </div>
+      </div>
+      </section>
     </>
-  )
-}
+  );
+};
 
-export default Section2
+export default Section2;
