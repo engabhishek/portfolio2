3f0a65bee86e168141d14702f1473af02f7367d2
Abhishek Sharma <abhisheksharma879150@gmail.com>
2025-03-14 22:26:52 +0530

    complete section 1 an add in photo or about me section

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import Section1 from './Components/Section1'
 import H1 from './Components/General/H1'
+import Marquee from './Components/General/Marquee'
+import Section2 from './Components/Section2'
+import Box from './Components/General/Box'
 
 
 const App = () => {
   return (
     <>
     <Section1/>
-    
+    <Marquee text={'open to work'}/>
+    <Section2/>
     </>
   )
 }
