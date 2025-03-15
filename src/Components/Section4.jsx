2a222c5d2f8e08f4f4ce792887cba747ec413641
Abhishek Sharma <abhisheksharma879150@gmail.com>
2025-03-15 13:25:00 +0530

    formate document

--- src/Components/Section4.jsx
+++ src/Components/Section4.jsx
@@ -1,26 +1,31 @@
-import React from 'react'
-import H1 from './General/H1'
+import React from "react";
+import H1 from "./General/H1";
 
 const Section4 = () => {
   return (
     <>
-    <section className='section4 px-2'>
-    <div>
+      <section className="section4 px-2">
         <div>
-        <H1 text = {"Get In Touch"}/>
-        <p className="text-white font-mono my-4 text-xl ">
-        Let's build something amazing together! 🚀 Whether you have a project idea 💡, want to collaborate 🤝, or just want to say hello 👋, I'd love to hear from you. Drop me a message ✉️, and let's make things happen! 🎯
-        </p>
-        <div className='inline-block w-70 '>
-            <img className='rounded-4xl' src="src\assets\get in tuch.webp" alt="" />
+          <div>
+            <H1 text={"Get In Touch"} />
+            <p className="text-white font-mono my-4 text-xl ">
+              Let's build something amazing together! 🚀 Whether you have a
+              project idea 💡, want to collaborate 🤝, or just want to say hello
+              👋, I'd love to hear from you. Drop me a message ✉️, and let's
+              make things happen! 🎯
+            </p>
+            <div className="inline-block w-70 ">
+              <img
+                className="rounded-4xl"
+                src="src\assets\get in tuch.webp"
+                alt=""
+              />
+            </div>
+          </div>
         </div>
-        </div>
-    </div>
-
-    </section>
-
+      </section>
     </>
-  )
-}
+  );
+};
 
-export default Section4
+export default Section4;
